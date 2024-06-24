431dd1b665a6da737d2f19af0a83d95400af3e3b
Ramachandran, Roobun (External) <roobun.ramachandran-p@brightspeed.com>
2024-06-24 17:07:33 +0530

    404 Page added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,58 +1,59 @@
 export const runtime = "edge";
+import GradientText from "@/components/GradientText/GradientText";
+import "./globals.css";
+import StyledText from "@/components/StyledText/StyledText";
+import Button from "@/components/Button/Button";
+import Link from "next/link";
 
 export default function NotFound() {
+
+  const ANIMATION_DURATION = 500;
   return (
     <>
-      <title>404: This page could not be found.</title>
-      <div style={styles.error}>
-        <div>
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
-            }}
-          />
-          <h1 className="next-error-h1" style={styles.h1}>
-            404
-          </h1>
-          <div style={styles.desc}>
-            <h2 style={styles.h2}>This page could not be found.</h2>
-          </div>
-        </div>
+    <div className="main-container" style={{gap:'20px'}}>
+
+    <div className="warningIcon">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" focusable="false" style={{ userSelect: 'none', width: '100%', height: '100%', display: 'inline-block', fill: 'rgb(255, 255, 255)', color: 'rgb(255, 255, 255)', flexShrink: 0 }}>
+          <g color="rgb(255, 255, 255)">
+            <path d="M240.26,186.1,152.81,34.23h0a28.74,28.74,0,0,0-49.62,0L15.74,186.1a27.45,27.45,0,0,0,0,27.71A28.31,28.31,0,0,0,40.55,228h174.9a28.31,28.31,0,0,0,24.79-14.19A27.45,27.45,0,0,0,240.26,186.1Zm-20.8,15.7a4.46,4.46,0,0,1-4,2.2H40.55a4.46,4.46,0,0,1-4-2.2,3.56,3.56,0,0,1,0-3.73L124,46.2a4.77,4.77,0,0,1,8,0l87.44,151.87A3.56,3.56,0,0,1,219.46,201.8ZM116,136V104a12,12,0,0,1,24,0v32a12,12,0,0,1-24,0Zm28,40a16,16,0,1,1-16-16A16,16,0,0,1,144,176Z"></path>
+          </g>
+          <g color="rgb(255, 255, 255)">
+            <path d="M240.26,186.1,152.81,34.23h0a28.74,28.74,0,0,0-49.62,0L15.74,186.1a27.45,27.45,0,0,0,0,27.71A28.31,28.31,0,0,0,40.55,228h174.9a28.31,28.31,0,0,0,24.79-14.19A27.45,27.45,0,0,0,240.26,186.1Zm-20.8,15.7a4.46,4.46,0,0,1-4,2.2H40.55a4.46,4.46,0,0,1-4-2.2,3.56,3.56,0,0,1,0-3.73L124,46.2a4.77,4.77,0,0,1,8,0l87.44,151.87A3.56,3.56,0,0,1,219.46,201.8ZM116,136V104a12,12,0,0,1,24,0v32a12,12,0,0,1-24,0Zm28,40a16,16,0,1,1-16-16A16,16,0,0,1,144,176Z"></path>
+          </g>
+        </svg>
       </div>
+      <h3 className="GreenText">ERROR 404 - PAGE NOT FOUND</h3>
+
+      <GradientText
+          element="h1"
+          className="text-4xl font-medium"
+          style={{
+            animation: `comeFromBottom ${ANIMATION_DURATION}ms`,
+            opacity: 1,
+            animationFillMode: "forwards",
+            animationDelay: "0s",
+            fontSize:"45px",
+            lineHeight:'3.5rem'
+          }}
+        >
+          Hey there, Sherlock! Unfortunately, this page is not the elusive clue you were hunting for.
+        </GradientText>
+
+        <StyledText fontSize="22px">
+        It seems to have vanished into thin air. But don't lose hope! Keep exploring the vast web of mysteries and you might stumble upon the treasure you seek.
+        </StyledText>
+
+        <Link href="/">
+        <Button
+          style={{
+            width:'160px',
+            flexDirection:'row-reverse',
+          }}
+          label="Back Home"
+          arrowDirection='left'
+        />
+        </Link>
+    </div>
     </>
   );
 }
-
-const styles = {
-  error: {
-    fontFamily:
-      'system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
-    height: "100vh",
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  desc: {
-    display: "inline-block",
-  },
-
-  h1: {
-    display: "inline-block",
-    margin: "0 20px 0 0",
-    padding: "0 23px 0 0",
-    fontSize: 24,
-    fontWeight: 500,
-    verticalAlign: "top",
-    lineHeight: "49px",
-  },
-
-  h2: {
-    fontSize: 14,
-    fontWeight: 400,
-    lineHeight: "49px",
-    margin: 0,
-  },
-} as const;
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,9 +5,10 @@ interface iButton {
   label: string;
   className?: string;
   style?: CSSProperties;
+  arrowDirection?:string;
 }
 
-const Button: React.FC<iButton> = ({ label, className = "", style = {} }) => {
+const Button: React.FC<iButton> = ({ label, arrowDirection, className = "", style = {} }) => {
 
   const addStyle = className + "font-bold py-2 px-4 rounded flex justify-center w-full ButtonContainer";
 
@@ -19,8 +20,12 @@ const Button: React.FC<iButton> = ({ label, className = "", style = {} }) => {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
       >
+        {arrowDirection === 'left' ?
+         <path d="M3 10a.997.997 0 01.293-.707l6-6a.999.999 0 011.414 0 .999.999 0 010 1.414L5.414 9H17a1 1 0 010 2H5.414l4.293 4.293a.999.999 0 11-1.414 1.414l-6-6A.997.997 0 013 10z" />
+        :
         <path d="M17 10a.997.997 0 01-.293.707l-6 6a.999.999 0 01-1.414 0 .999.999 0 010-1.414L14.586 11H3a1 1 0 010-2h11.586l-4.293-4.293a.999.999 0 111.414-1.414l6 6c.195.195.293.451.293.707z" />
-      </svg>
+      }
+        </svg>
     </button>
   );
 };
